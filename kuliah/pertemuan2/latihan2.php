240de698223a79f761c97938361528c977ad261e
Nasywa Salma <salmanasywa96@gmail.com>
2025-02-17 10:55:08 +0700

    selesai

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nama = "Nasywa Salma";
+    $email = "salmanasywa96@gmail.com";
+    $pekerjaan = "mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "UNPAS";
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+
+    echo "<p>Halo, nama saya $nama saya seorang $pekerjaan di $jurusan $universitas</p>";
+
+?>    
